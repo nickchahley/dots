fa96779de1f7ec81e5e490c5692cd21d4b463b58
nickchahley <nickchahley@gmail.com>
2024-09-10 16:10:34 -0400

    conda install location now optional arg, add pip

--- .config/yadm/setup/miniconda.sh
+++ .config/yadm/setup/miniconda.sh
@@ -1,11 +1,12 @@
 # Bootstrap conda env
 # Assumption is that paths added by `conda init` are already in shell configs
-CONDA_INSTALL="$HOME/.miniconda3"
+CONDA_INSTALL=${1:-"$HOME/.miniconda3"}
 mkdir -p "$CONDA_INSTALL"
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$CONDA_INSTALL"/miniconda.sh
 bash "$CONDA_INSTALL"/miniconda.sh -b -u -p "$CONDA_INSTALL"
 rm -rf "$CONDA_INSTALL"/miniconda.sh
 
-"$CONDA_INSTALL/bin/conda" update conda
-"$CONDA_INSTALL/bin/conda" install python
+# "$CONDA_INSTALL/bin/conda" update conda
+"$CONDA_INSTALL/bin/conda" install python pip
 "$CONDA_INSTALL/bin/conda" init
+
