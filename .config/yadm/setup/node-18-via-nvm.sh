24374b632247de731af3595b92fc8f34b94d8404
nickchahley <nickchahley@gmail.com>
2026-02-03 16:24:10 -0500

    typo

--- .config/yadm/setup/node-18-via-nvm.sh
+++ .config/yadm/setup/node-18-via-nvm.sh
@@ -1,6 +1,6 @@
 node_ver=${1:-"18"}
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.config/nvm" 
-"$NVIM_DIR/nvm.sh"
+"$NVM_DIR/nvm.sh"
 nvm install $node_ver
 nvm use $node_ver
