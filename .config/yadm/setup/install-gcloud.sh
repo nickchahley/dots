518c9bf336b5de302c22aff2e535134e42d7b8e6
nickchahley <nickchahley@gmail.com>
2026-03-24 11:14:43 -0400

    track install-gcloud

---
+++ .config/yadm/setup/install-gcloud.sh
@@ -0,0 +1,4 @@
+# https://docs.cloud.google.com/sdk/docs/install-sdk#linux
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-linux-x86_64.tar.gz && rm google-cloud-cli-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
