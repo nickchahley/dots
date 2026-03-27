40972b69a3302cbf125958b1dcaa7d325fe5adf0
nickchahley <nickchahley@gmail.com>
2026-03-27 09:21:21 -0400

    fix paths

--- .config/yadm/setup/nvim-dap-python.sh
+++ .config/yadm/setup/nvim-dap-python.sh
@@ -5,14 +5,10 @@
 sudo apt install -y python3-venv
 
 # nvim dap
-D="$(pwd)"
 VENV="$HOME/.virtualenvs"
 if ! [[ -d "$VENV" ]]; then
 	mkdir "$VENV"
 fi
-cd "$VENV"
 python3 -m venv debugpy
-debugpy/bin/python -m pip install -y debugpy
-cd "$D"
-
-debugpy/bin/python -m pip install -y pytest
+pyexec="$VENV"/debugpy/bin/python
+yes | "$pyexec" -m pip install debugpy pytest
