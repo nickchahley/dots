2595c0883dc40b681dca181f3d5d4f242e85821b
nickchahley <nickchahley@gmail.com>
2026-04-24 17:07:27 -0400

    gcloud utils install

--- .config/yadm/setup/install-gcloud.sh
+++ .config/yadm/setup/install-gcloud.sh
@@ -2,3 +2,8 @@
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-linux-x86_64.tar.gz && rm google-cloud-cli-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
+
+# prompts to login via browser link
+# need to paste code into terminal
+# then select cloud project
+./google-cloud-sdk/bin/gcloud init
